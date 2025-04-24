cb72e041b033e611d3e210c0e719ee7b13f787cb
ir109 <rob20072@byui.edu>
2025-04-24 11:19:47 -0600

    Initial for name assignment.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("Input your first name: ");
+        string name_first = Console.ReadLine();
+        Console.Write("Input your last name: ");
+        string name_last = Console.ReadLine();
+        Console.WriteLine($"Your name is {name_last}, {name_first} {name_last}.");
+
+        int x = 10;
+        if (x == 10)
+        {
+            Console.WriteLine("x is 10.")
+        }
     }
 }
