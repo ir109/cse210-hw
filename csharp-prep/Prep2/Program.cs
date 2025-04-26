3f15b0f6fc9d998c06fc92a0d13e33b6188cfdde
ir109 <ianrobbins09@gmail.com>
2025-04-26 16:51:09 -0600

    Initial if-else statement for letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string gradePercent = Console.ReadLine();
+
+        int grade = int.Parse(gradePercent);
+        
+        if (grade >= 90)
+        {
+            Console.WriteLine("You got an A.");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You got a B.");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You got a C. You passed the class!");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You got a D. You did not pass the class. :(");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class. :(");
+        }
     }
 }
