33acaaa62881999f9371959b6e20c49d3e02c785
ir109 <ianrobbins09@gmail.com>
2025-04-29 11:13:20 -0600

    Prints one line 20 times

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -11,5 +11,10 @@ class Program
         }
 
         Console.WriteLine("Hello, boyos.");
+
+        for(int i = 0; i < 20; i++)
+        {
+            Console.WriteLine("Hello, Joe.");
+        }
     }
 }
