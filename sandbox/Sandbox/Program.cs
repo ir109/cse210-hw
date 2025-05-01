5b768bf153c61d9436906d9c27458b1637eacd70
ir109 <ianrobbins09@gmail.com>
2025-05-01 11:08:42 -0600

    New function to add numbers

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,19 +2,45 @@ using System;
 
 class Program
 {
+
+    static double AddNumbers(double a, double b)
+    {
+        return a + b;
+    }
+
     static void Main(string[] args)
     {
-        int x = 10;
-        if (x == 10)
-        {
-            Console.WriteLine("x is 10.");
-        }
+        // int x = 10;
+        // if (x == 10)
+        // {
+        //     Console.WriteLine("x is 10.");
+        // }
 
-        Console.WriteLine("Hello, boyos.");
+        // Console.WriteLine("Hello, boyos.");
 
-        for(int i = 0; i < 20; i++)
-        {
-            Console.WriteLine($"Hello, Joe. {i}");
-        }
+        // for(int i = 0; i < 20; i++)
+        // {
+        //     Console.WriteLine($"Hello, Joe. {i}");
+        // }
+
+        // int x = 0;
+        // int y;
+        // x++;
+        // y = x;
+        // Console.WriteLine(x);
+        // Console.WriteLine(y);
+
+        // List<int> myNumbers = new List<int>();
+        // myNumbers.Add(99);
+        // myNumbers.Add(2);
+        // myNumbers.Add(45);
+        // myNumbers.Add(356);
+        
+        // foreach(int i in myNumbers)
+        // {
+        //     Console.WriteLine(i);
+
+        double total = AddNumbers(123.98, 984.234);
+        Console.WriteLine(total);
     }
 }
