12fb2d6dfdd7440b8d14c59a40ce916d3e5ec16c
ir109 <ianrobbins09@gmail.com>
2025-05-03 18:14:00 -0600

    Core requirements and stretch challenges complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string replay = "y";
+
+        while(replay == "y")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+            int guessCount = 0;
+            int guess = -1;
+            while(guess != number)
+            {
+                Console.Write("Guess the number: ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You got it in {guessCount} guesses!");
+                }
+            }
+            Console.Write("Do you want to play again? y/n: ");
+            replay = Console.ReadLine();  
+        }
+        Console.WriteLine("Thanks for playing! Here's a cookie! (There are no cookies)");
     }
 }
