c6b5e9ec2ae247c9d54dab46733d306cf9f4dc38
ir109 <ianrobbins09@gmail.com>
2025-05-03 18:41:37 -0600

    Core requirements and stretch challenges complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,63 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while(userNumber != 0)
+        {
+            Console.Write("Enter a number (press 0 to end): ");
+
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+
+            if(userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        
+        int sum = 0;
+        foreach(int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"\nSum: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"Average: {average}");
+
+        int max = numbers[0];
+        foreach(int number in numbers)
+        {
+            if(number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"Largest number: {max}");
+
+        //Stretch Challenges
+
+        int min = numbers[0];
+        foreach(int number in numbers)
+        {
+            if(number < min && number > 0)
+            {
+                min = number;
+            }
+        }
+        Console.WriteLine($"Smallest positive number: {min}");
+
+        numbers.Sort();
+
+        Console.WriteLine("Sorted numbers: ");
+        foreach(int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
