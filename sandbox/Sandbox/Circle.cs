1b1bdf1da6e36a14101608363fb0f760a4e80733
ir109 <ianrobbins09@gmail.com>
2025-05-09 14:56:29 -0600

    Implemented cylinder class

---
+++ sandbox/Sandbox/Circle.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Circle
+{
+    private double _radius; //can only be used in "class Circle"
+    public void SetRadius(double radius) //can be used outside of "class Circle"
+    {
+        if(radius < 0)
+        {
+            Console.WriteLine("Error. Radius must be > 0");
+            return;
+        }
+        _radius = radius; //"_radius" is the attribute, "radius" os the parameter
+    }
+    public double GetRadius()
+    {
+        return _radius;
+    }
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
