1e65b5a5fc4eb160899a69ac3d853f72b39dbfa5
ir109 <ianrobbins09@gmail.com>
2025-05-21 17:38:01 -0600

    Initial submission

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()}");
+        Console.WriteLine($"Decimal form: {fraction1.GetDecimalValue()}");
+
+        Fraction fraction2 = new Fraction(6);
+        Console.WriteLine($"Fraction 2: {fraction2.GetFractionString()}");
+        Console.WriteLine($"Decimal form: {fraction2.GetDecimalValue()}");
+
+        Fraction fraction3 = new Fraction(6, 8);
+        Console.WriteLine($"Fraction 3: {fraction3.GetFractionString()}");
+        Console.WriteLine($"Decimal form: {fraction3.GetDecimalValue()}");
     }
 }
