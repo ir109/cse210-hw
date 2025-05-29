d86dd90be1db76654484549ec503b29a4d69bbfb
ir109 <ianrobbins09@gmail.com>
2025-05-29 10:18:43 -0600

    Made WordCounter class

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        WordCounter wordCounter = new WordCounter();
+    }
+}
+    
+    
---
+++ WordCounter/WordCounter.cs
@@ -0,0 +1,26 @@
+class WordCounter
+{
+    private List<string> _words;
+
+    public WordCounter(string text)
+    {
+        _words = new List<string>();
+        SplitTextIntoWords(text);
+    }
+    private SplitTextIntoWords(string text)
+    {
+        string[] words = text.Split(' ');
+        foreach (string word in words)
+        {
+            _words.Add(word);
+        }
+    }
+    public void DisplayWords()
+    {
+        foreach (string word in _words)
+        {
+            Console.Writeline(word);
+        }
+
+    }
+}
