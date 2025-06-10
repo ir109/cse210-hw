b02ce7af96634765e31b7ecc8f74614dc497ac38
ir109 <ianrobbins09@gmail.com>
2025-06-10 11:08:36 -0600

    Spinner and countdown test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int duration = 9;
+        DateTime currentTime = DateTime.Now;
+        DateTime endTime = currentTime.AddSeconds(duration);
+        int sleepTime = 350;
+        int count = duration;
+
+        //countdown
+        while (DateTime.Now < endTime)
+        {
+            Console.Write(count--);
+            Thread.Sleep(1000);
+            Console.Write("\b");
+        }
+
+        //spinner
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("+");
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
+            Console.Write("-");
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
+        }
+    }
+}
+
+    
