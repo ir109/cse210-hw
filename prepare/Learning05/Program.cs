412e40fe7084c39474af98eb8127ae567ff1834f
ir109 <ianrobbins09@gmail.com>
2025-06-17 18:36:23 -0600

    Learning05 submission

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> myShape = new List<Shape>();
+        myShape.Add(new Square(7, "blue"));
+        myShape.Add(new Rectangle(4, 2, "red"));
+        myShape.Add(new Circle(9, "yellow"));
+
+        foreach (Shape shape in myShape)
+        {
+            DisplayShape(shape);
+        }
+    }
+    public static void DisplayShape(Shape shape)
+    {
+        Console.Write(shape.GetArea());
+        Console.WriteLine(shape.GetColor());
     }
 }
