924f39e5fccbc3efbb070bc5c5d0a158c3444e65
ir109 <ianrobbins09@gmail.com>
2025-07-08 10:19:22 -0600

    Develop05 menu

--- prove/Develop05/Menu_Develop05;.cs
+++ prove/Develop05/Menu_Develop05;.cs
@@ -1,9 +1,9 @@
-namespace Develop05;
-
+using System;
 public class Menu
 {
     public int DisplayMenu()
     {
+        int menuInput;
         string menu =
 @$"Selection:
 1. Add Goal
@@ -14,11 +14,50 @@ public class Menu
 6. Record Event
 0. Exit
 ";
-        Console.WriteLine(menu);
-        return int.Parse(Console.ReadLine());
+        do
+        {
+            Console.WriteLine(menu);
+            menuInput = int.Parse(Console.ReadLine());
+            switch (menuInput)
+            {
+                case 1:
+                    Console.WriteLine("Case 1\n");
+                    //add goal
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2\n");
+                    //save
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3\n");
+                    //load
+                    break;
+                case 4:
+                    Console.WriteLine("Case 4\n");
+                    //display goals
+                    break;
+                case 5:
+                    Console.WriteLine("Case 5\n");
+                    //display score
+                    break;
+                case 6:
+                    Console.WriteLine("Case 6\n");
+                    //record event
+                    break;
+                case 0:
+                    Console.WriteLine("Exiting");
+                    //exit
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection.\n");
+                    break;
+            }
+        } while (menuInput != 0);
+        return menuInput;
     }
-    public int DisplayCreateGoalMenu()
-    {
+        
+    // public int DisplayCreateGoalMenu()
+    // {
 
-    }
+    // }
 }
